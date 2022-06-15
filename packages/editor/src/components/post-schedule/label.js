96e35a1ad74ba3bb24fc47e485d4515cedda4527
Robert Anderson <robert@noisysocks.com>
2022-06-15 10:30:43 +1000

    wip

--- packages/editor/src/components/post-schedule/label.js
+++ packages/editor/src/components/post-schedule/label.js
@@ -1,28 +1,72 @@
 /**
+ * External dependencies
+ */
+import moment from 'moment';
+
+/**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { format, __experimentalGetSettings } from '@wordpress/date';
-import { withSelect } from '@wordpress/data';
+import { __, _x, sprintf } from '@wordpress/i18n';
+import { __experimentalGetSettings, dateI18n } from '@wordpress/date';
+import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import { store as editorStore } from '../../store';
 
-export function PostScheduleLabel( { date, isFloating } ) {
-	const settings = __experimentalGetSettings();
-	return date && ! isFloating
-		? format(
-				`${ settings.formats.date } ${ settings.formats.time }`,
-				date
-		  )
-		: __( 'Immediately' );
+export function getPostScheduleLabel( date, isFloating ) {
+	if ( ! date || isFloating ) {
+		return __( 'Immediately' );
+	}
+
+	const dateMoment = moment.tz( date, 'WP' );
+
+	// If the user's timezone differs from the site's timezone, words like
+	// 'Tomorrow' are probably going to be confusing, so just display the full
+	// date and timezone.
+	if ( dateMoment.utcOffset() !== moment().utcOffset() ) {
+		return dateI18n(
+			_x( 'F j, Y g:i a P', 'date format with timezone offset' ),
+			dateMoment
+		);
+	}
+
+	const nowMoment = moment.tz( 'WP' );
+
+	const { formats } = __experimentalGetSettings();
+
+	if ( dateMoment.isSame( nowMoment, 'day' ) ) {
+		return sprintf(
+			// translators: %s: Time of day the post is scheduled for.
+			__( 'Today at %s' ),
+			dateI18n( formats.time, dateMoment )
+		);
+	}
+
+	if ( dateMoment.isSame( nowMoment.clone().add( 1, 'day' ), 'day' ) ) {
+		return sprintf(
+			// translators: %s: Time of day the post is scheduled for.
+			__( 'Tomorrow at %s' ),
+			dateI18n( formats.time, dateMoment )
+		);
+	}
+
+	if ( dateMoment.isSame( nowMoment, 'year' ) ) {
+		return dateI18n(
+			_x( 'F j g:i a', 'date format without year' ),
+			dateMoment
+		);
+	}
+
+	return dateI18n( formats.datetime, dateMoment );
 }
 
-export default withSelect( ( select ) => {
-	return {
+export default function PostScheduleLabel() {
+	const { date, isFloating } = useSelect( ( select ) => ( {
 		date: select( editorStore ).getEditedPostAttribute( 'date' ),
 		isFloating: select( editorStore ).isEditedPostDateFloating(),
-	};
-} )( PostScheduleLabel );
+	} ) );
+
+	return getPostScheduleLabel( date, isFloating );
+}
